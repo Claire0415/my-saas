4a35965bac826bbae324f4c574d42f674d8f06a8
liangyuetong <liangyuetong@IT-LAP0630204.local>
2025-09-22 19:38:05 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {},
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        // 可以在这里添加全局Less变量
+        // additionalData: `@import "@/styles/variables.less";`
+      },
+    },
+  },
+  server: {
+    host: "127.0.0.1",
+    port: 8000,
+  },
+});
